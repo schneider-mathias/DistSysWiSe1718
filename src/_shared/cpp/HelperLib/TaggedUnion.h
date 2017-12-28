01e7f540b5f471ffae9329771f02eff9a8f2dcd5
Fettstorch <Matt&&91>
2017-12-28 12:36:30 +0100

    - remove wstring in taggedunion (currently unused and makes problems for initialization)

--- src/_shared/cpp/HelperLib/TaggedUnion.h
+++ src/_shared/cpp/HelperLib/TaggedUnion.h
@@ -11,8 +11,7 @@ struct TaggedUnion
 		Boolean,
 		Char,
 		Int,
-		Double,
-		WString
+		Double
 	};
 
 
@@ -20,7 +19,6 @@ struct TaggedUnion
 	TaggedUnion(const char& value) : type(Char), value(value) {}
 	TaggedUnion(const int& value) : type(Int), value(value) {}
 	TaggedUnion(const double& value) : type(Double), value(value) {}
-	TaggedUnion(const std::wstring& value) : type(WString), value(value) {}
 	TaggedUnion(const TaggedUnion& ref) : type(ref.type), value(ref.value) {}
 
 
@@ -29,8 +27,6 @@ struct TaggedUnion
 	char getChar() const { assert(type == Char); return value.getChar(); }
 	int getInt() const { assert(type == Int);  return value.getInt(); }
 	double getDouble() const { assert(type == Double);  return value.getDouble(); }
-	std::wstring getWString() const { assert(type == WString);  return value.getWString(); }
-
 private:
 
 	union Union
@@ -38,14 +34,12 @@ private:
 		Union(const bool& value) : booleanValue(value) {}
 		Union(const char& value) : charValue(value) {}
 		Union(const int& value) : intValue(value) {}
-		Union(const double& value) : doubleValue(value) {}
-		Union(const std::wstring& value) : wstringValue(value) {}
+		Union(const double& value) : doubleValue(value){}
 		Union(const Union& ref) : 
 			booleanValue(ref.booleanValue), 
 			charValue(ref.charValue),
 			intValue(ref.intValue),
-			doubleValue(ref.doubleValue),
-			wstringValue(ref.wstringValue)
+			doubleValue(ref.doubleValue)
 			{}
 		~Union() {}
 
@@ -56,7 +50,6 @@ private:
 			charValue = ref.charValue;
 			intValue = ref.intValue;
 			doubleValue = ref.doubleValue;
-			wstringValue = ref.wstringValue;
 			return *this;
 		}
 
@@ -65,7 +58,6 @@ private:
 		char getChar() const { return charValue; }
 		int getInt() const { return intValue; }
 		double getDouble() const { return doubleValue; }
-		std::wstring getWString() const { return wstringValue; }
 
 	private:
 		bool	booleanValue;
