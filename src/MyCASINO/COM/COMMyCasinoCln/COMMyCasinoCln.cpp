ea04bb72de823f9985aa0b7d51f19ea782d98064
Fettstorch <Matt&&91>
2017-12-29 14:10:23 +0100

    - add CmdInterpreter dependencies to RPC/COM clients

--- src/MyCASINO/COM/COMMyCasinoCln/COMMyCasinoCln.cpp
+++ src/MyCASINO/COM/COMMyCasinoCln/COMMyCasinoCln.cpp
@@ -14,81 +14,8 @@
 #include "BstrStringConverter.h"
 
 
-BOOL AuthTest()
-{
-	CAuthService* AuthService = new CAuthService(&std::wstring(L"USERDATA"));
-	// Important: Set project property -> Debugging -> Working directory -> $(TargetDir)
-	// in order to use relative paths during debugging!
-	if (!AuthService->readRegisteredUser(L"test.txt"))
-	{
-		std::cout << "Cannot read Test.txt" << std::endl;
-		return FALSE;
-	}
-
-	ULONG mathiasSessionId;
-	if (!AuthService->login(L"Mathias", L"Passwort", &mathiasSessionId))
-	{
-		std::cout << "First login failed" << std::endl;
-		return FALSE;
-	}
-
-	if (!AuthService->login(L"Mathias", L"Passwort", &mathiasSessionId))
-	{
-		std::cout << "Second login failed" << std::endl;
-		return FALSE;
-	}
-
-
-	if (!AuthService->isLoggedIn(mathiasSessionId))
-	{
-		std::cout << "First user should be logged in" << std::endl;
-		return FALSE;
-	}
-
-
-	if (AuthService->login(L"Mathias", L"InvalidPasswort", &mathiasSessionId))
-	{
-		std::cout << "Login worked with wrong password" << std::endl;
-		return FALSE;
-	}
-
-	ULONG manuelSessionId;
-	if (!AuthService->login(L"Manuel", L"Passwort", &manuelSessionId))
-	{
-		std::cout << "Second user login failed" << std::endl;
-		return FALSE;
-	}
-
-	if (!AuthService->logout(manuelSessionId))
-	{
-		std::cout << "Second user logout failed" << std::endl;
-		return FALSE;
-	}
-
-	if (AuthService->logout(manuelSessionId))
-	{
-		std::cout << "Second user is logged out but could log out" << std::endl;
-		return FALSE;
-	}
-
-
-	delete AuthService;
-
-	return TRUE;
-}
-
-
 int main(int argc, char**argv)
 {
-	// UTs
-	if (!AuthTest())
-	{
-		std::cout << "Error in AuthService test. Aborting." << std::endl;
-		return FALSE;
-	}
-
-
-
 	CoInitialize(NULL);
 	IClassFactory *pCF = NULL;
 	
