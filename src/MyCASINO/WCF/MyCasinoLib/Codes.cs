092498a0b1be93f167f7054d596c1f890ec4d546
Fettstorch <Matt&&91>
2018-01-11 17:49:11 +0100

    - add class for calling cpp dll which reolves errorcodes

---
+++ src/MyCASINO/WCF/MyCasinoLib/Codes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MyCasinoLib
+{
+    /// <summary>
+    /// https://limbioliong.wordpress.com/2011/06/16/returning-strings-from-a-c-api/
+    /// </summary>
+    public class Codes
+    {
+        [DllImport("MyCasinoDefines.dll", EntryPoint = "ResolveCode", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern string ResolveCode(long code);
+    }
+}
