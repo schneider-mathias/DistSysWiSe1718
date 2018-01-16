b061794166e9ee07522053a739df4c6ba019d89c
Fettstorch <Matt&&91>
2018-01-16 09:52:28 +0100

    - finalize comments

--- src/MyCASINO/_shared/cpp/MyCasinoDefines/MyCasinoResolveCodes.cpp
+++ src/MyCASINO/_shared/cpp/MyCasinoDefines/MyCasinoResolveCodes.cpp
@@ -2,25 +2,29 @@
 // project:	MyCasinoDefines
 // file:	MyCasinoResolveCodes.cpp
 //
-// summary:	Implements my casino resolve codes class
+// summary:	Method for resolving MyCasino class based on defines. This method will be 
+//			shared (as .DLL) with managed code (.NET) in order to have the same messages in the 
+//			WCF client.
 //
 //			Copyright (c) 2018 OTH-Amberg/Weiden. All rights reserved.
 //
 //			Date		Developer			Change
-//			13.01.2018	Mathias Schneider	Created
+//			10.01.2018	Mathias Schneider	Created
+//			XXXXXXXXXX	Mathias Schneider	Changed
  *-----------------------------------------------------------------------------------------------**/
 
 #include <Windows.h>
 #include "MyCasinoDefines.h"
 #include "CharStringConverter.h"
 
-//https://limbioliong.wordpress.com/2011/06/16/returning-strings-from-a-c-api/
 extern "C"
 {
 	/**--------------------------------------------------------------------------------------------------
-	 * <summary>	Resolve code. </summary>
+	 * <summary>	Resolves a MyCasino HRESULT code and returns a string to callee.
+	 * 				Inspired by https://limbioliong.wordpress.com/2011/06/16/returning-strings-from-a-c-api/
+	 * 				 </summary>
 	 *
-	 * <param name="code">	The code. </param>
+	 * <param name="code">	The return code which should be resolved. </param>
 	 *
 	 * <returns>	Null if it fails, else a pointer to a char. </returns>
 	 *-----------------------------------------------------------------------------------------------**/
@@ -40,7 +44,6 @@ extern "C"
 		strcpy(pszReturn, szSampleString);
 		delete[] szSampleString;
 
-		// Return pszReturn.
 		return pszReturn;
 	}
 }
